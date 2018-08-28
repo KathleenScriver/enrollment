72db3df559d53cbbdc5eab7bf8fccabbc14cfdb0
Kathleen Yruegas <kathleenecn@gmail.com>
2018-08-28 13:38:28 -0600

    Adds test to add new student

---
+++ spec/features/students/user_can_create_new_student_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+describe "As a user" do
+  describe "I visit student index page" do
+    it "should be able to create new student" do
+
+      student_name = "Kathleen Scriver"
+      visit students_path
+      click_link("Create New Student")
+
+      expect(current_path).to eq(new_student_path)
+
+      fill_in :student_name, with: student_name
+      click_on("Create Student")
+
+      expect(current_path).to eq(student_path(Student.last))
+      expect(page).to have_content(student_name)
+    end
+  end
+end
