a3afbfc811ad016e8a4ac3343616b68175b84a46
Kathleen Yruegas <kathleenecn@gmail.com>
2018-08-28 14:38:25 -0600

    Adds address model and student/address relationships

---
+++ app/models/address.rb
@@ -0,0 +1,8 @@
+class Address < ApplicationRecord
+  validates_presence_of :description,
+                        :street_address,
+                        :city,
+                        :state,
+                        :zip_code
+  belongs_to :student
+end
